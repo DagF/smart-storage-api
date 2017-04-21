4d581c5adc6fde3d53003b4e651976acb639f511
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2017-04-21 08:19:55 +0200

    Added filter

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,10 +5,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register('box/box', box_views.BoxViewSet)
-router.register('box/activities', box_views.ActivityViewSet)
-router.register('box/rfid', box_views.RFIDViewSet)
 router.register('box/items', box_views.ItemViewSet)
 router.register('box/users', box_views.UserViewSet)
-router.register('box/weight', box_views.WeightViewSet)
+router.register('box/activities', box_views.ActivityViewSet, 'activities')
+router.register('box/rfid', box_views.RFIDViewSet, 'rfid')
+router.register('box/weight', box_views.WeightViewSet, 'weight')
 
 urlpatterns = router.urls
--- src/box/api/views.py
+++ src/box/api/views.py
@@ -1,10 +1,11 @@
 import django_filters.rest_framework
+from django.utils.dateparse import parse_datetime
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from .serializers import BoxSerializer, ActivitySerializer, WeightSerializer, RFIDSerializer, ItemSerializer, UserSerializer
+from .serializers import BoxSerializer, ActivitySerializer, WeightSerializer, RFIDSerializer, ItemSerializer, \
+    UserSerializer
 from ..models import Box, Activity, RFID, Item, User, Weight
 
 
@@ -13,20 +14,28 @@ class BaseViewSet(GenericViewSet, RetrieveModelMixin):
     authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication)
 
 
+class DateFilterable(BaseViewSet):
+    # TODO this is done wrong but it works
+    # how it should be done!!! /?newer_than=2017-03-28T17:21:02.135853Z
+    # http://www.django-rest-framework.org/api-guide/filtering/#specifying-a-filterset
+    def get_queryset(self):
+        created_raw = self.request.query_params.get('newer_than', None)
+        if created_raw:
+            created = parse_datetime(created_raw)
+            queryset = self.model.objects.filter(created__gt=created)
+            print("\n\n\n")
+            print(created)
+            print("\n\n\n")
+        else:
+            queryset = self.model.objects.all()
+
+        return queryset
+
+
 class BoxViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
     queryset = Box.objects.all()
     serializer_class = BoxSerializer
     lookup_field = "name"
-
-
-class ActivityViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
-    queryset = Activity.objects.all()
-    serializer_class = ActivitySerializer
-
-
-class RFIDViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
-    queryset = RFID.objects.all()
-    serializer_class = RFIDSerializer
 
 
 class ItemViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
@@ -39,6 +48,16 @@ class UserViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMi
     serializer_class = UserSerializer
 
 
-class WeightViewSet(BaseViewSet, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
-    queryset = Weight.objects.all()
+class ActivityViewSet(DateFilterable, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
+    model = Activity
+    serializer_class = ActivitySerializer
+
+
+class RFIDViewSet(DateFilterable, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
+    model = RFID
+    serializer_class = RFIDSerializer
+
+
+class WeightViewSet(DateFilterable, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin):
+    model = Weight
     serializer_class = WeightSerializer
